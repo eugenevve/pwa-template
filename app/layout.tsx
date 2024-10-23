7539a03cd851d034af8ce7305168694825bdc24c
Eugene <88931804+eugenevve@users.noreply.github.com>
2024-10-23 19:40:10 +0600

    Update size

--- app/layout.tsx
+++ app/layout.tsx
@@ -22,7 +22,7 @@ export default function RootLayout({
         <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
 
         {/* Website icons */}
-        <link rel="icon" href="/favicons/favicon.ico" sizes="32x32" />
+        <link rel="icon" href="/favicons/favicon.ico" />
         <link rel="icon" href="/favicons/favicon.svg" type="image/svg+xml" />
         <link rel="apple-touch-icon" href="/favicons/favicon-180-180.png" />
 
