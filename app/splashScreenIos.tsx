74fd2681aa30098ddc4c8b6b9d772e5b62c6f5c6
Eugene <88931804+eugenevve@users.noreply.github.com>
2024-10-24 00:21:57 +0600

    fix_test

---
+++ app/splashScreenIos.tsx
@@ -0,0 +1,10 @@
+"use client";
+import { useEffect } from "react";
+import iosPWASplash from "@/utils/iosPwaSplash";
+
+export default function SplashScreenIos() {
+  useEffect(() => {
+    iosPWASplash("/icons/favicons/favicon-512-512.png", "#37474F");
+  }, []);
+  return null;
+}
